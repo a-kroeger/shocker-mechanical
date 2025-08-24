0bec82bb0302ccb0f86cfa527230796e75a8c56d
Alexander <alex@detourdigital.ca>
2025-08-24 15:30:30 -0600

    Set up footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css"
 import Navigation from "@/components/global/Navigation.js"
 import Footer from "@/components/global/Footer.js"
-import { fetchAllServices } from "@/utils/contentful"
+import { fetchAllServices, getSiteContent } from "@/utils/contentful"
 
 export const metadata = {
   title: "Shocker Mechanical",
@@ -11,13 +11,14 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   // Fetch all service entries once on the server
   const services = await fetchAllServices()
+  const content = await getSiteContent()
 
   return (
     <html lang="en">
       <body>
         <Navigation services={services} />
         {children}
-        <Footer />
+        <Footer data={content} />
       </body>
     </html>
   )
--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import style from "./global.module.css";
+import Link from "next/link";
+import scriptLogo from "../../assets/script-logo.jpg";
+import facebook from "../../assets/facebook.png";
+import instagram from "../../assets/instagram.png";
 
-export default function footer() {
+export default function footer(props) {
   return (
-    <div>footer</div>
-  )
+    <div>
+      <Image
+        className={style.footerLogo}
+        src={scriptLogo}
+        width={100}
+        height={100}
+        alt='Shocker Mechanical Inc.'
+      ></Image>
+      <div className={style.footerMain}>
+        <ul className={style.quickLinks}>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/services">Services</Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+        </ul>
+        <div className={style.socials}>
+          <Link href={props.data.fields.instagramLink}>
+            <Image alt='Instagram' src={instagram}></Image>
+          </Link>
+          <Link href={props.data.fields.facebookLink}>
+            <Image alt='Facebook' src={facebook}></Image>
+          </Link>
+        </div>
+      </div>
+      <div className={style.copyright}>
+        <div>Shocker Mechanical 2025</div>
+      </div>
+    </div>
+  );
 }
